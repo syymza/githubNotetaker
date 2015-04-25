184ba05cb3872527f2e3ea6af8a748ff8b5433d3
syymza <syymza@gmail.com>
2015-04-25 16:07:18 +0100

    Added Main (NavigatorIOS) Component.

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,33 @@
 'use strict';
 
 var React = require('react-native');
+var Main = require('./App/Components/Main')
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View,
 } = React;
 
 class githubNotetaker extends  React.Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Github Notetaker!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute = {{
+          title: 'Github Notetaker',
+          component: Main
+        }} />
     );
   }
 };
 
 var styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#111111'
+  }
 });
 
 AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
